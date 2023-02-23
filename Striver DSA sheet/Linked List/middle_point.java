7e8603fae495ab47d210e6b30dee4a837eded496
sanjaykumar0324 <114232171+sanjaykumar0324@users.noreply.github.com>
2023-02-23 22:01:48 +0530

    Create middle_point.java

---
+++ Striver DSA sheet/Linked List/middle_point.java
@@ -0,0 +1,14 @@
+ public static Node<Integer> midPoint(Node<Integer> head) {
+	        //Your code goes here
+	        if (head==null || head.next==null)
+	        {
+	            return head;
+	        }
+	        Node<Integer> fast=head,slow=head;
+	        while(fast.next!=null && fast.next.next!=null)
+	        {
+	            slow=slow.next;
+	            fast=fast.next.next;
+	        }
+	        return slow;
+	    }
